55e4cafbb0f7a1f91d28cf58a4bf51ed01b30ed7
Hung Bui Van <hungbv@Hungs-MacBook-Pro.local>
2020-12-09 15:21:48 +0700

    initial project

---
+++ CircleCITutorial/CircleCITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleCITutorialApp.swift
+//  CircleCITutorial
+//
+//  Created by Hung Bui Van on 09/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CircleCITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
